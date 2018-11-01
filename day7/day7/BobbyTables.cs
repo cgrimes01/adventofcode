8b9ffe402e30b9e6a1eae9a160c97771634d6f4f
ciaran <chfgrimes@gmail.com>
2018-11-01 08:50:46 +0000

    Add start of day7

---
+++ day7/day7/BobbyTables.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7
+{
+    public class BobbyTables
+    {
+        public List<Computation> computationList = new List<Computation>();
+
+        public BobbyTables(string filePointer)
+        {
+            string line;
+
+            StreamReader file = new StreamReader(filePointer);
+            while ((line = file.ReadLine()) != null)
+            {
+                computationList.Add(new Computation(line));
+            }
+        }
+
+        public UInt16 CalculateValue(string value) {
+            string calculation = computationList.Find(x => x.OutputWire == value.Trim()).Calculation;
+            string[] split;
+            string operation = "";
+
+            if (calculation.IndexOf("AND") != -1)
+            {
+                operation = "AND";
+                split = calculation.Split(new[] { operation }, StringSplitOptions.None);
+                return (UInt16)(CalculateValue(split[0]) & CalculateValue(split[1]));
+            }
+            else if(calculation.IndexOf("OR") != -1) {
+                operation = "OR";
+                split = calculation.Split(new[] { operation }, StringSplitOptions.None);
+                return (UInt16)(CalculateValue(split[0]) | CalculateValue(split[1]));
+            }
+            else if (calculation.IndexOf("NOT") != -1)
+            {
+                operation = "NOT";
+                split = calculation.Split(new[] { operation }, StringSplitOptions.None);
+                return (UInt16)(~ CalculateValue(split[1]));
+            }
+            else if (calculation.IndexOf("LSHIFT") != -1)
+            {
+                operation = "LSHIFT";
+                split = calculation.Split(new[] { operation }, StringSplitOptions.None);
+                return (UInt16)(CalculateValue(split[0]) << Int16.Parse(split[1]));
+            }
+            else if (calculation.IndexOf("RSHIFT") != -1)
+            {
+                operation = "RSHIFT";
+                split = calculation.Split(new[] { operation }, StringSplitOptions.None);
+                return (UInt16)(CalculateValue(split[0]) >> Int16.Parse(split[1]));
+            }
+
+            if(operation == "")
+            {
+                UInt16 number;
+                if(UInt16.TryParse(calculation, out number))
+                {
+                    return number;
+                } else
+                {
+                    CalculateValue(calculation);
+                }
+            }
+
+            return (UInt16)0;
+        }
+}
+
+    public class Computation
+    {
+        public string OutputWire { get; set; }
+        public string Calculation { get; set; }
+
+        public Computation(string computationText)
+        {
+            string[] split = computationText.Split(new[] { " -> " }, StringSplitOptions.None);
+            OutputWire = split[1];
+            Calculation = split[0];
+        }
+    }
+
+}
---
+++ day7/day7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BobbyTables program = new BobbyTables(@"D:\AdventOfCode\adventofcode2015\day7\day7input.txt");
+            Console.WriteLine(program.CalculateValue("a"));
+            Console.ReadLine();
+        }
+    }
+}
