d72071c8e7caab7c7024938784e93021fdd18787
ciaran <chfgrimes@gmail.com>
2018-12-12 21:45:38 +0000

    Add JSON parsing and start of part2 for day 12

--- day12/Day12/Program.fs
+++ day12/Day12/Program.fs
@@ -3,6 +3,7 @@
 open System
 open System.Text.RegularExpressions
 open System.IO
+open FSharp.Data
 
 let getNumbers (numbersString:string) = 
     Regex.Split(numbersString, "[^0-9-]+")
@@ -16,10 +17,18 @@ let addNumbers (numbers:seq<int>) =
 let sumAllNumbers (input:string) =
     addNumbers (getNumbers input)
 
+let rec cleanObject (input:JsonValue) =
+    match input.AsArray().Length with
+    | 0 -> ""
+    | _ -> if input.TryGetProperty("red").IsNone then "" else [ for v in input -> cleanObject v] |> List.toArray |> string
+
 let fileInputPart1 = String.Concat (File.ReadAllLines @"D:\AdventOfCode\adventofcode2015\day12\day12input.txt")
+let jsonInput = JsonValue.Parse(fileInputPart1)
 
 [<EntryPoint>]
 let main argv =
     printfn "PART 1:"
     printfn "%i" (sumAllNumbers fileInputPart1)
+    printfn "PART 2:"
+    printfn "%s" (cleanObject (JsonValue.Parse("""{"red": 1}""")))
     0 // return an integer exit code
