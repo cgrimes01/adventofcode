a3b2f27a887cac8eb838bd6730cf3733d31a7547
Ciaran Grimes <chfgrimes@gmail.com>
2020-12-07 22:41:49 +0000

    Extract get_max into separate function

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -2,13 +2,39 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    part1(&input);
+    let max = get_max(&input);
+    part1(&input, max);
+    // part2(&input, max);
 }
 
-fn part1(input : &String) {
+// struct Slope {
+//     right : u32,
+//     down: u32,
+// }
+
+// fn part2(input : &String, max : i32) {
+//     let slopes = vec![
+//         Slope { right: 1, down: 1},
+//         Slope { right: 3, down: 1},
+//         Slope { right: 5, down: 1},
+//         Slope { right: 7, down: 1},
+//         Slope { right: 1, down: 2},
+//     ];
+//     let mut trees : i32 = 0;
+//     let mut x : i32 = 0;
+    
+//     for line in input.lines() {
+//         if line.chars().nth(x as usize).unwrap() == '#' {
+//             trees = trees + 1;
+//         }
+//         x = toboggan(max, x, 3);
+//     }
+//     dbg!(trees);
+// }
+
+fn part1(input : &String, max : i32) {
     let mut trees : i32 = 0;
     let mut x : i32 = 0;
-    let max = (input.find('\n').unwrap() as i32) - 2;
     for line in input.lines() {
         if line.chars().nth(x as usize).unwrap() == '#' {
             trees = trees + 1;
@@ -16,6 +42,10 @@ fn part1(input : &String) {
         x = toboggan(max, x, 3);
     }
     dbg!(trees);
+}
+
+fn get_max(input : &String) -> i32 {
+    (input.find('\n').unwrap() as i32) - 2
 }
 
 fn toboggan(max : i32, original_x : i32, x_change : i32) -> i32 {
