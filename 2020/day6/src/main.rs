c039ee0b481b0619284fa7f91f60e50ca33508b2
Ciaran Grimes <chfgrimes@gmail.com>
2021-01-09 20:55:43 +0000

    2020 day 6 part 2

--- 2020/day6/src/main.rs
+++ 2020/day6/src/main.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    println!("Sum of counts = {}", part1(&input));
-    // println!("Valid passports part 2 = {}", part2(&input));
+    println!("Sum of distinct counts = {}", part1(&input));
+    println!("Sum of all counts = {}", part2(&input));
 }
 
 fn part1(input : &String) -> usize{
@@ -27,4 +27,61 @@ fn part1(input : &String) -> usize{
     }
     total = total + group.len();
     total
+}
+
+fn part2(input : &String) -> usize{
+    let mut group = HashMap::new();
+    let mut total = 0;
+    let mut first_group = true;
+    for line in input.lines() {
+        match line {
+            "" => {
+                total = total + group.len();
+                group = HashMap::new();
+                first_group = true;
+            }
+            _ => {
+                match first_group {
+                    true => {
+                        for c in line.chars() {
+                            group.insert(c, c);    
+                        }
+                        first_group = false;
+                        
+                    }
+                    false => {
+                        let mut new_group = HashMap::new();
+                        for c in line.chars() {
+                            if group.contains_key(&c) {
+                                new_group.insert(c, c);
+                            }         
+                        }
+                        group = new_group;
+                    }
+                }
+            }
+        }
+    }
+    total = total + group.len();
+    total
+}
+
+#[cfg(test)]
+mod day2_tests {
+    use super::*;
+    #[test]
+    fn group_1() {
+        let input = "abc".to_string();  
+        assert_eq!(part2(&input), 3);
+    }
+    #[test]
+    fn group_2() {
+        let input = "a\nb\nc".to_string();  
+        assert_eq!(part2(&input), 0);
+    }
+    #[test]
+    fn group_3() {
+        let input = "ab\nac".to_string();  
+        assert_eq!(part2(&input), 1);
+    }
 }
