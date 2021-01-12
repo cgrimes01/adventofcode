e7ac333970e392b3983da6c2c55197fbc3e225a1
Ciaran Grimes <chfgrimes@gmail.com>
2021-01-12 23:28:40 +0000

    2020 day 7 , refactor to return bags Vec

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -14,25 +14,45 @@ struct Bag {
 }
 
 fn part1(input : &String) -> usize {
-    // let mut bags = Vec::new();
+    let mut bags: Vec<Bag> = Vec::new();
     for line in input.lines() {
-        dbg!(process_bag_line(&line));
+        bags = process_bag_line(&line, bags);
     }
+    dbg!(bags);
     5
 }
 
-fn process_bag_line(input : &str) -> Bag {
-    let mut bag = Bag { colour: "".to_string(), contents: HashMap::new()};
+fn append_word(current : &mut String, append : &str) {
+    if current != "" {
+        current.push_str(" ");
+    }
+    current.push_str(append);
+}
+
+// fn get_bags_containing(colour : String, bags : HashMap<String, Bag>) -> HashMap<String, Bag> {
+//     let mut containingbags = HashMap::new();
+
+//     for bag in bags {
+//         if bag
+//     }
+//     containingbags
+// }
+
+fn process_bag_line(input : &str, mut bags: Vec<Bag>) -> Vec<Bag> {
     let mut state = "colour";
-    let mut contained_colour = "".to_string();
+    let mut contained_colour = String::from("");
+    let mut container_colour = String::from("");
     let mut contained_count = 0;
+    let mut contents = HashMap::new();
     for word in input.split(" ") {
         match state {
             "colour" => {
                 match word {
                     "bags" => {},
                     "contain" => state = "contains",
-                    _ => bag.colour = (bag.colour + " " + word).trim().to_string(),
+                    _ => {
+                        append_word(&mut container_colour, word);
+                    }
                 }
             },
             "contains" => {
@@ -43,18 +63,19 @@ fn process_bag_line(input : &str) -> Bag {
                     Err(_) => {
                         if word.contains("bag") {
                             if contained_count != 0 {
-                                bag.contents.insert(contained_colour.trim().to_string(), contained_count);
+                                contents.insert(contained_colour, contained_count);
                             }
                             contained_colour = "".to_string();
                             contained_count = 0;
                         } else {
-                            contained_colour = contained_colour + " " + word;
+                            append_word(&mut contained_colour, word);
                         }
                     },
                 };
-            },
+            }, 
             _ => {},
         }
     }
-    bag
+    bags.push(Bag { colour: container_colour.clone(), contents: contents});
+    bags
 }
