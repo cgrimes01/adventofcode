09a14c8a2d12585149030e96f80711f0ff5f2507
Ciaran Grimes <chfgrimes@gmail.com>
2021-01-31 16:09:31 +0000

    2020 day 9 part2

--- 2020/day9/src/main.rs
+++ 2020/day9/src/main.rs
@@ -7,10 +7,12 @@ fn main() {
         numbers.push(line.parse().unwrap());
     };
 
-    println!("First invalid number = {}", find_invalid_number(&numbers, 25));
+    let invalid_number = get_invalid_number(&numbers, 25);
+    println!("First invalid number = {}", invalid_number);
+    println!("Encryption weakness = {}", get_encryption_weakness(&numbers, invalid_number));
 }
 
-fn find_invalid_number(numbers: &Vec<u64>, preamble_length: usize) -> u64 {
+fn get_invalid_number(numbers: &Vec<u64>, preamble_length: usize) -> u64 {
     let mut invalid_found = false; 
     let mut current_index : usize = preamble_length;
     while !invalid_found {
@@ -35,8 +37,48 @@ fn find_invalid_number(numbers: &Vec<u64>, preamble_length: usize) -> u64 {
     numbers[current_index]
 }
 
+fn get_encryption_weakness(numbers: &Vec<u64>, target_total: u64) -> u64 {
+    let range = find_range_equal_to_total(numbers, target_total);
+    sum_min_and_max(&range)
+}
+
+fn find_range_equal_to_total(numbers: &Vec<u64>, target_total: u64) -> Vec<u64> {
+    let mut range_found = false; 
+    let mut current_index : usize = 0;
+    let mut ending_index : usize = 0;
+
+    while !range_found {
+        let mut current_total : u64 = 0;
+        for x in current_index..numbers.len() - 1  {
+            current_total = current_total + numbers[x];
+            
+            if current_total == target_total {
+                range_found = true;
+                ending_index = x;
+                break;
+            }
+
+            if current_total > target_total {
+                break;
+            }
+        }
+
+        if !range_found {
+            current_index = current_index + 1;
+        }
+    }
+    numbers[current_index..=ending_index].to_vec()
+}
+
+fn sum_min_and_max(numbers : &Vec<u64>) -> u64 {
+    let min = numbers.iter().min().unwrap();
+    let max = numbers.iter().max().unwrap();
+
+    max + min
+}
+
 #[cfg(test)]
-mod part1_tests {
+mod day9_tests {
     use super::*;
     #[test]
     fn part_1() {
@@ -46,7 +88,19 @@ mod part1_tests {
             numbers.push(line.parse().unwrap());
         };
 
-        let output = find_invalid_number(&numbers, 5);
+        let output = get_invalid_number(&numbers, 5);
         assert_eq!(output, 127);
+    }
+
+    #[test]
+    fn part_2() {
+        let input = fs::read_to_string("testinput.txt").unwrap();
+        let mut numbers : Vec<u64> = Vec::new();
+        for line in input.lines() {
+            numbers.push(line.parse().unwrap());
+        };
+
+        let output = get_encryption_weakness(&numbers, 127);
+        assert_eq!(output, 62);
     }
 }
